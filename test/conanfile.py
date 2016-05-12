554ae1fc222a3a03a05105df5f8ee4a45b117c89
Lars Bilke <lars.bilke@ufz.de>
2016-05-12 09:07:44 +0200

    Run conan test in correct working directory.

--- test/conanfile.py
+++ test/conanfile.py
@@ -19,4 +19,4 @@ class TiffReuseConan(ConanFile):
         self.copy(pattern="*.dylib", dst="bin", src="lib")
 
     def test(self):
-        self.run(os.sep.join([".","bin", "test"]))
+        self.run("cd bin && ./test")
